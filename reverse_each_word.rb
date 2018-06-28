498de0c899471a10019db79ddcfa87277d9966b8
Alejandro Aguilar <alejandro.aguilar.jr@gmail.com>
2018-06-28 03:38:37 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   array= string.split
-  array.collect do |x|
+  new_array.collect { |x| x.reverse}
     new_string.push(x.reverse)
   end
   new_string = new_string.join(" ")
